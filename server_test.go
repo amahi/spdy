f37a075efe852a922a07a9da6a2f59b0ef246727
nileshjagnik <nileshjagnik@gmail.com>
2014-06-27 16:40:29 +0530

    redundant tests removed

--- server_test.go
+++ server_test.go
@@ -189,28 +189,3 @@ func TestTLSServerNoNPN(t *testing.T) {
 	server.Close()
 	time.Sleep(100 * time.Millisecond)
 }
-
-func TestTLSServerNoNPN(t *testing.T) {
-	http.HandleFunc("/", ServerHandler)
-	go ListenAndServeTLSNoNPN("localhost:4040", SERVER_CERTFILE, SERVER_KEYFILE, nil)
-	time.Sleep(200 * time.Millisecond)
-	cert, err := tls.LoadX509KeyPair(CLIENT_CERTFILE, CLIENT_KEYFILE)
-	if err != nil {
-		fmt.Printf("server: loadkeys: %s", err)
-	}
-	config := tls.Config{Certificates: []tls.Certificate{cert}, InsecureSkipVerify: true} //, NextProtos: []string{"spdy/3"}}
-	conn, err := tls.Dial("tcp", "127.0.0.1:4040", &config)
-	if err != nil {
-		fmt.Printf("client: dial: %s", err)
-	}
-	client, err := spdy.NewClientConn(conn)
-	handle(err)
-	req, err := http.NewRequest("GET", "http://localhost:4040/banana", nil)
-	handle(err)
-	res, err := client.Do(req)
-	handle(err)
-	data := make([]byte, int(res.ContentLength))
-	_, err = res.Body.(io.Reader).Read(data)
-	fmt.Println(string(data))
-	res.Body.Close()
-}
