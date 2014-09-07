0430a6de5897a485211bd2811917e7f0cfb8097b
Carlos Puchol <cpg+git@amahi.org>
2014-09-07 01:10:26 -0700

    update date

--- header.go
+++ header.go
@@ -1,4 +1,4 @@
-// Copyright 2013, Amahi.  All rights reserved.
+// Copyright 2013-14, Amahi. All rights reserved.
 // Use of this source code is governed by the
 // license that can be found in the LICENSE file.
 
